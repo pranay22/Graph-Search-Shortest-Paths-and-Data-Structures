e6859cb91c54b5c981d6c0740a9856684165ff00
Pranay <pranaysarkar22@gmail.com>
2018-08-23 15:36:24 +0200

    SCC python modified - faster execution

--- Week_1/SCC.py
+++ Week_1/SCC.py
@@ -1,123 +1,63 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import time
-import datetime
-import sys
-
-start = time.time()
-print datetime.datetime.now()
-
-with open('SCC.txt') as f:
-    #a = [[int(x) for x in ln.split()] for ln in f]
-    data_set_u = []
-    data_set_v = []
-    for ln in f:
-        if len(ln) >1:
-            u,v = ln.split()
-            u = int(u)
-            v = int(v)            
-            data_set_u.append(u)
-            data_set_v.append(v)
-f.close()
-
-print 'Open file time: '+ str(time.time() - start) + 's'
-print datetime.datetime.now()
-    
-sys.setrecursionlimit((max(data_set_u+data_set_v)+ len(data_set_u))*100)
-
-def DFS_Loop():
-    num = max(data_set_u+data_set_v)
-    
-    start_time_DFS_Loop = time.time()
-    global t
-    t = 0
-    global s
-    s = None
-    global visited
-    visited = [False]* num
-    global leader
-    leader = [None] * num
-    global f
-    f = [None] * num
-    
-    
-    for i in range(num,0,-1):
-        if visited[i-1] == False:
-            s = i
-            DFS(i)
-    print 'End with func DFS_Loop() time: '+ str(time.time() - start_time_DFS_Loop)+ 's'
-    print 'End with func DFS_Loop() whole time: '+ str(time.time() - start)+ 's'    
-# For debug
-#print data_set_u
-#print data_set_v
-    
-            
-def DFS(node):
-    start_time_DFS = time.time()
-    
-    global t
-    visited[node-1] = True
-    #print visited
-    #print visited
-    leader[node-1] = s
-    #print leader
-    arc = []
-    arc = [data_set_v[i] for i,x in enumerate(data_set_u) if x==node] 
-    #print arc
-    for i in arc:
-        if visited[i-1]==0:
-            DFS(i)
-
-    t+=1
-    f[node-1] = t
-    print 'End with func DFS time: '+ str(time.time() - start_time_DFS)+ 's'
-    print 'End with func DFS whole time: '+ str(time.time() - start)+ 's'
-    
-DFS_Loop()
-print 'DFS_Loop time: '+ str(time.time() - start)+ 's'
-
-
-##reverse tail and head data
-
-rev_u,rev_v = data_set_v,data_set_u
-new_u = [None] * (len(rev_u))
-new_v = [None] * (len(rev_v))
-
-for i,val in enumerate(f):
-    for i_v,val_v in enumerate(rev_v):
-        if val_v == i+1:
-            new_v[i_v] = val
-    
-    for i_u,val_u in enumerate(rev_u):
-        if val_u == i+1:
-            new_u[i_u] = val    
-    
-data_set_u = new_u
-data_set_v = new_v
-
-print 'Reverse data time: '+ str(time.time() - start)+ 's'
-
-DFS_Loop()
-print 'DFS_Loop time: '+ str(time.time() - start)+ 's'
-
-# Print leader
-# Calculate repeated times appearancing in leader list
-
-count_list = [0]*len(leader)
-indices = [0]*len(leader)
-
-i_count_list = 0
-while len(leader) > 0:
-    count_list[i_count_list] = leader.count(leader[0])
-    indices = [i for i, x in enumerate(leader) if x == leader[0]]
-
-    for i in xrange(len(indices)):
-        del leader[leader.index(leader[0])]
-    i_count_list = i_count_list+1
-print 'calc time: '+ str(time.time() - start)+ 's'
-
-sorted_count_list = sorted(count_list, key=int, reverse=True)
-print '5 largest SCC sizes are:'
-print sorted_count_list[0:5]
-print datetime.datetime.now()
+import sys,threading  
+sys.setrecursionlimit(3000000)  
+threading.stack_size(67108864)  
+  
+def firstdfs(vertexind):  
+    global fs,isexplored,visitordered,mapDictT  
+    if len(mapDictT[vertexind])>0:  
+        for ind in mapDictT[vertexind]:  
+            if not isexplored[ind-1]:  
+                isexplored[ind-1]=True  
+                firstdfs(ind)  
+    visitordered[fs-1]=vertexind  
+    #print(str(vertexind)+' fs: '+str(fs))  
+    fs=fs-1  
+  
+def seconddfs(vertexind):  
+    global s,secisexplored,header,mapDict  
+    if len(mapDict[vertexind])==0:return  
+    for ind in mapDict[vertexind]:  
+        if not secisexplored[ind-1]:  
+            secisexplored[ind-1]=True  
+            seconddfs(ind)  
+    header[s-1]+=1  
+  
+def sccmain():  
+    global mapDict,mapDictT,fs,isexplored,visitordered,s,secisexplored,header  
+    maplength=875714   
+    f=open('SCC.txt','r')  
+    mapDict={x:[] for x in range(1,maplength+1)}  
+    mapDictT={x:[] for x in range(1,maplength+1)}  
+    for line in f.readlines():  
+        tmp=[int(x) for x in line.split()]  
+        mapDict[tmp[0]].append(tmp[1])  
+        mapDictT[tmp[1]].append(tmp[0])  
+    f.close  
+  
+    fs=maplength  
+    isexplored=[False for x in range(1,maplength+1)]  
+    secisexplored=[False for x in range(1,maplength+1)]  
+    visitordered=[0 for x in range(1,maplength+1)]  
+    header=[0 for x in range(1,maplength+1)]  
+  
+    for ind in range(1,maplength+1):  
+        if not isexplored[ind-1]:  
+            isexplored[ind-1]=True  
+            firstdfs(ind)  
+    print('Second DFS')  
+    for ind in visitordered:  
+        if not secisexplored[ind-1]:  
+            s=ind  
+            secisexplored[ind-1]=True  
+            seconddfs(ind)  
+  
+    header.sort(reverse=True) 
+    print('5 largest SCC sizes are:')
+    print(header[0:5])  
+  
+if __name__ =='__main__':  
+    thread=threading.Thread(target=sccmain)  
+    thread.start() 
